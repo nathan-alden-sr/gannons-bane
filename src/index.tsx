da8bf47d236d0dfe730df4dfa860553c9a47c97b
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2022-08-05 20:51:59 -0400

    Disable console output in production

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// Disable console output in production
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
